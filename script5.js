4688f2f69f79c67d0ee8498f60ec0e0e42694ec2
SonYk1993 <anti-magedemon@mail.ru>
2023-04-27 10:42:38 +0300

    change function BtnColor

--- script5.js
+++ script5.js
@@ -1,6 +1,7 @@
-
-
-
+// const firstSection = document.querySelector('.header')
+// const button1 = document.querySelector('.carusel_button1')
+// const button2 = document.querySelector('.carusel_button2')
+// const button3 = document.querySelector('.carusel_button3')
 
 // function changeBgImage(){
 //     if(firstSection.classList.contains('header_2') || firstSection.classList.contains('header_2') ){
@@ -32,37 +33,42 @@
 // button3.addEventListener('click', changeBgImage2)
 //btnColorActive
 
-const firstSection = document.querySelector('.header')
-const button1 = document.querySelector('.carusel_button1')
-const button2 = document.querySelector('.carusel_button2')
-const button3 = document.querySelector('.carusel_button3')
 
-function changeBtnColor(){
-    if(button1.classList.contains('btnColorActive')){
-        button2.classList.add('btnColorActive')
-        button3.classList.remove('btnColorActive')
-    }
-    else{
-        button1.classList.add('btnColorActive')
-        button2.classList.remove('btnColorActive')
-        button3.classList.remove('btnColorActive')
-    }
+
+// function changeBtnColor(){
+//     if(button1.classList.contains('btnColorActive')){
+//         button2.classList.add('btnColorActive')
+//         button3.classList.remove('btnColorActive')
+//     }
+//     else{
+//         button1.classList.add('btnColorActive')
+//         button2.classList.remove('btnColorActive')
+//         button3.classList.remove('btnColorActive')
+//     }
    
-}
-function changeBtnColor2(){
-    button2.classList.add('btnColorActive')
-    button1.classList.remove('btnColorActive')
-    button3.classList.remove('btnColorActive')
-}
-function changeBtnColor3(){
-    button3.classList.add('btnColorActive')
-    button1.classList.remove('btnColorActive')
-    button2.classList.remove('btnColorActive')
-}
+// }
+// function changeBtnColor2(){
+//     button2.classList.add('btnColorActive')
+//     button1.classList.remove('btnColorActive')
+//     button3.classList.remove('btnColorActive')
+// }
+// function changeBtnColor3(){
+//     button3.classList.add('btnColorActive')
+//     button1.classList.remove('btnColorActive')
+//     button2.classList.remove('btnColorActive')
+// }
 
-button1.addEventListener('click', changeBtnColor)
-button2.addEventListener('click', changeBtnColor2)
-button3.addEventListener('click', changeBtnColor3)
+// button1.addEventListener('click', changeBtnColor)
+// button2.addEventListener('click', changeBtnColor2)
+// button3.addEventListener('click', changeBtnColor3)
+
+
+const buttons = document.querySelectorAll('.carusel_button')
+function changeBtnColor(event){
+    buttons.forEach(elem => elem.classList.remove('btnColorActive'))
+    event.target.classList.add('btnColorActive')
+}
+buttons.forEach((elem)=> elem.addEventListener('click', changeBtnColor))
 
 
 
